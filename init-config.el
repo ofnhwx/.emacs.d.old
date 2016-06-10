fe8b80b3f4fa0994dfc48e2a2aa145d4991a70bb
Yuta Fujita <ofnhwx@komunan.net>
2016-06-10 13:28:14 +0900

    update: 末尾に勝手に改行を付与しない

--- init-config.el
+++ init-config.el
@@ -1,7 +1,7 @@
 ﻿;;; init-enhance.el --- 環境回りの設定.
 ;;
 ;; -*- mode: Emacs-Lisp; coding: utf-8 -*-
-;; Last updated: <2016/06/09 17:39:52>
+;; Last updated: <2016/06/10 13:28:01>
 ;;
 
 ;;; Commentary:
@@ -85,6 +85,7 @@
  '(resize-mini-windows    t)    ;; ミニバッファ拡大防止
  '(mouse-drag-copy-region nil)  ;; 選択範囲を勝手にコピーしない
  '(create-lockfiles       nil)  ;; lockfileを作成しない
+ '(require-final-newline  nil)  ;; 勝手に改行を追加しない
  ;; -------------------------------------------------------------------- ;;
  )
 
