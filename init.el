e44c8c29bc7f0aa79fd40f090ad30ce714829e58
Yuta Fujita <ofnhwx@komunan.net>
2016-07-04 12:30:44 +0900

    update: 'projectile'の設定ファイルを:localに移動
    あとキーマップの設定がダメっぽい
    https://github.com/bbatsov/projectile/issues/991

--- init.el
+++ init.el
@@ -260,7 +260,13 @@
 (use-package "projectile"
   :ensure t
   :diminish projectile-mode
+  :init
+  (custom-set-variables
+   '(projectile-keymap-prefix (kbd) "C-x p"))
   :config
+  (custom-set-variables
+   '(projectile-known-projects-file (e:expand "projectile-bookmarks.eld" :local))
+   '(projectile-cache-file (e:expand "projectile.cache" :local)))
   (defun helm-find-files-with-projectile (&optional arg)
     (interactive "P")
     (if (projectile-project-p)
