d10ff11922b0cf2a8433d202105a352d9b53e713
Yuta Fujita <ofnhwx@komunan.net>
2017-04-21 10:51:21 +0900

    update: 自分用拡張を少し整理

--- init-enhance.el
+++ init-enhance.el
@@ -1,7 +1,7 @@
 ;;; init-enhance.el --- 個人設定用の拡張機能.
 ;;
 ;; -*- mode: Emacs-Lisp; coding: utf-8 -*-
-;; Last updated: <2017/04/20 15:42:43>
+;; Last updated: <2017/04/21 10:44:12>
 ;;
 
 ;;; Commentary:
@@ -28,31 +28,33 @@
 (defun os-type-win-p   () "Windows." (eq os-type os-type-win  ))
 
 ;; define:`e:require'
-(defmacro e:require (package &optional noerror)
-  `(cl-eval-when (compile load eval)
-     (require ,package nil ,noerror)))
+(defun e:require (package &optional noerror)
+  ""
+  (require package nil noerror))
 
 ;; define:`e:require-package'
-(defmacro e:require-package (package &optional load noerror)
-  `(when (e:require 'package t)
-     (condition-case err
-         (or (package-installed-p ,package)
-             (package-install ,package))
-       (error (message "%s" err)))
-     (if ,load
-         (e:require ,package ,noerror)
-       (package-installed-p ,package))))
+(defun e:require-package (package &optional load noerror)
+  ""
+  (when (e:require 'package t)
+    (condition-case err
+        (or (package-installed-p package)
+            (package-install package))
+      (error (message "%s" err)))
+    (if load
+        (e:require package noerror)
+      (package-installed-p package))))
 
 ;; define:`e:load-config'
-(defmacro e:load-config (filename &optional local)
-  `(let ((file ,(if local `(e:expand ,(concat "config/" filename) :local)
-                  `(e:expand ,filename :conf))))
-     ,(when local
-        '(progn (unless (file-exists-p (file-name-directory file))
-                  (make-directory (file-name-directory file) t))
-                (unless (file-exists-p (concat (file-name-sans-extension file) ".el"))
-                  (write-region "" nil (concat (file-name-sans-extension file) ".el")))))
-     (load file)))
+(defun e:load-config (filename &optional local)
+  ""
+  (let ((file (if local (e:expand (concat "config/" filename) :local)
+                (e:expand filename :conf))))
+    (when local
+      (unless (file-exists-p (file-name-directory file))
+        (make-directory (file-name-directory file) t))
+      (unless (file-exists-p (concat (file-name-sans-extension file) ".el"))
+        (write-region "" nil (concat (file-name-sans-extension file) ".el"))))
+    (load file)))
 
 ;; use:`package'
 (when (e:require 'package t)
@@ -71,10 +73,11 @@
   (defmacro use-package (&rest args)))
 
 ;; define:`e:set-font'
-(defmacro e:set-font (fontname height)
-  `(when (find-font (font-spec :name ,fontname))
-     (set-face-attribute 'default nil :family ,fontname :height ,height)
-     t))
+(defun e:set-font (fontname height)
+  ""
+  (when (find-font (font-spec :name fontname))
+    (set-face-attribute 'default nil :family fontname :height height)
+    t))
 
 ;; define:`e:get-dir'
 (defvar e:get-dir-alist
@@ -87,25 +90,30 @@
     (:temp (e:unexpand (file-truename (e:expand ".emacs" temporary-file-directory))))
     (:user user-emacs-directory)))
 (defun e:get-dir (dirtype)
+  ""
   (let ((e (cadr (assoc dirtype e:get-dir-alist))))
     (if e (eval e) dirtype)))
 
 ;; define:`e:expand'
 (defun e:expand (filename &optional dirtype)
+  ""
   (let ((directory (e:get-dir dirtype)))
     (expand-file-name filename directory)))
 
 ;; define:`e:unexpand'
 (defun e:unexpand (filename)
+  ""
   (abbreviate-file-name filename))
 
 ;; define:`e:safe-exec'
 (defmacro e:safe-exec (sexplist)
+  ""
   `(if (fboundp (car ',sexplist))
        ,sexplist))
 
 ;; define:`e:loaded'
 (defun e:loaded ()
+  ""
   (when load-in-progress
     (message "*Loading %s...done"
              (file-name-sans-extension (file-name-nondirectory load-file-name)))))
