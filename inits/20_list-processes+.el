252091643d14243ddfa85eb711ae5f337e71acc9
Yuta Fujita <ofnhwx@komunan.net>
2017-04-24 18:04:46 +0900

    add: 'list-processes+'を導入(どちらかというとWindows用)

---
+++ inits/20_list-processes+.el
@@ -0,0 +1,15 @@
+;;; 20_list-processes+.el --- setup list-processes+.
+;;
+;; -*- mode: Emacs-Lisp; coding: utf-8 -*-
+;; Last updated: <2017/04/24 18:01:51>
+;;
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package list-processes+
+  :if (e:require-package 'list-processes+))
+
+(provide '20_list-processes+)
+;;; 20_list-processes+.el ends here
