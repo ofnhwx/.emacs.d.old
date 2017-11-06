7878e355f16b6c25feeec712e1f9f53709f124bd
Yuta Fujita <ofnhwx@komunan.net>
2017-11-06 18:28:19 +0900

    update: 'web-mode'のインデントを調整

--- inits/40_web-mode.el
+++ inits/40_web-mode.el
@@ -1,7 +1,7 @@
 ;;; 40_web-mode.el --- setup web-mode.
 ;;
 ;; -*- mode: Emacs-Lisp; coding: utf-8 -*-
-;; Last updated: <2017/04/24 12:56:26>
+;; Last updated: <2017/11/01 11:39:50>
 ;;
 
 ;;; Commentary:
@@ -19,18 +19,18 @@
   (set-variable 'web-mode-code-indent-offset   4)
   (set-variable 'web-mode-attr-indent-offset   2)
    ;; padding
-  (set-variable 'web-mode-style-padding  1)
-  (set-variable 'web-mode-script-padding 1)
-  (set-variable 'web-mode-block-padding  0)
+  (set-variable 'web-mode-style-padding  2)
+  (set-variable 'web-mode-script-padding 2)
+  (set-variable 'web-mode-block-padding  2)
   :config
   ;; インデント再調整
   (with-eval-after-load "editorconfig"
     (add-hook
      'editorconfig-custom-hooks
      (lambda (props)
-       (setq web-mode-script-padding 1)
-       (setq web-mode-style-padding  1)
-       (setq web-mode-block-padding  0)))))
+       (setq web-mode-script-padding 2)
+       (setq web-mode-style-padding  2)
+       (setq web-mode-block-padding  2)))))
 
 (provide '40_web-mode)
 ;;; 40_web-mode.el ends here
