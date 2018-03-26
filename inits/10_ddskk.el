7c7fe13e8e23e98332d9f615745eaab20f0633a4
Yuta Fujita <ofnhwx@komunan.net>
2018-03-26 17:47:59 +0900

    update: skkの設定にsticky-keyを追加

--- inits/10_ddskk.el
+++ inits/10_ddskk.el
@@ -1,7 +1,7 @@
 ;;; 10_ddskk.el --- setup template.
 ;;
 ;; -*- mode: Emacs-Lisp; coding: utf-8 -*-
-;; Last updated: <2018/01/28 00:37:18>
+;; Last updated: <2018/03/07 10:16:24>
 ;;
 
 ;;; Commentary:
@@ -13,7 +13,8 @@
   (set-variable 'skk-user-directory (e:expand "ddskk" :conf))
   (set-variable 'skk-byte-compile-init-file t)
   (set-variable 'skk-isearch-start-mode 'latin)
-  (set-variable 'skk-preload t))
+  (set-variable 'skk-preload t)
+  (set-variable 'skk-sticky-key ";"))
 
 (provide '10_ddskk)
 ;;; 10_ddskk.el ends here
