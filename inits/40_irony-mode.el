70aab1eac894e9ce6e7a7aa9bf7fa95d025963f9
Yuta Fujita <ofnhwx@komunan.net>
2018-04-04 16:10:51 +0900

    update: 'irony'の設定調整

--- inits/40_irony-mode.el
+++ inits/40_irony-mode.el
@@ -1,7 +1,7 @@
 ;;; 40_irony-mode.el --- setup template.
 ;;
 ;; -*- mode: Emacs-Lisp; coding: utf-8 -*-
-;; Last updated: <2018/03/29 16:03:44>
+;; Last updated: <2018/04/04 16:04:13>
 ;;
 
 ;;; Commentary:
@@ -11,9 +11,13 @@
 (use-package irony
   :init
   (set-variable 'irony-additional-clang-options '("-std=c++11"))
+  (set-variable 'irony-user-dir (e:expand "irony" :cache))
   :config
   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-  (add-hook 'c-mode-common-hook 'irony-mode))
+  (add-hook 'c-mode-common-hook 'irony-mode)
+  (defun irony-add-include (path)
+    (add-to-list 'irony-additional-clang-options (format "-I%s" path)))
+  (e:load-local-config "irony"))
 
 (provide '40_irony-mode)
 ;;; 40_irony-mode.el ends here
