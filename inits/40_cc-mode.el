c852ede85ad02e958d05abec8fb28df79b7818da
Yuta Fujita <ofnhwx@komunan.net>
2018-04-20 16:46:02 +0900

    add: C系のモードでSource/Headerの切り替えを実装

---
+++ inits/40_cc-mode.el
@@ -0,0 +1,28 @@
+;;; 40_cc-mode.el --- setup template.
+;;
+;; -*- mode: Emacs-Lisp; coding: utf-8 -*-
+;; Last updated: <2018/04/18 11:23:23>
+;;
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package cc-mode
+  :bind
+  (:map c++-mode-map
+        ("C-c /" . cc-switch-header/source))
+  :config
+  (defun cc-switch-header/source ()
+    (interactive)
+    (let* ((path (s-trim (shell-command-to-string "pwd")))
+           (relative (s-trim (shell-command-to-string "git rev-parse --show-cdup")))
+           (target (f-filename (buffer-file-name)))
+           (candidate-str (s-trim (shell-command-to-string (format "git ls-files %s | grep %s" relative (file-name-sans-extension target)))))
+           (candidate (--filter (not (string-equal target (f-filename it))) (--map (e:expand it path) (s-split "\n" candidate-str)))))
+      (if (zerop (length candidate))
+          (print "not found.")
+        (find-file (car candidate))))))
+
+(provide '40_cc-mode)
+;;; 40_cc-mode.el ends here
