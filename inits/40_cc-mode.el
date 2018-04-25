daedd0c639ad8507da877eb0c49ce435c9c66102
Yuta Fujita <ofnhwx@komunan.net>
2018-04-25 09:54:58 +0900

    update: C系のモードでSource/Headerの切り替えをより厳密に判定

--- inits/40_cc-mode.el
+++ inits/40_cc-mode.el
@@ -1,7 +1,7 @@
 ;;; 40_cc-mode.el --- setup template.
 ;;
 ;; -*- mode: Emacs-Lisp; coding: utf-8 -*-
-;; Last updated: <2018/04/23 10:54:26>
+;; Last updated: <2018/04/24 16:54:56>
 ;;
 
 ;;; Commentary:
@@ -18,7 +18,7 @@
     (let* ((path (s-trim (shell-command-to-string "pwd")))
            (relative (s-trim (shell-command-to-string "git rev-parse --show-cdup")))
            (target (f-filename (buffer-file-name)))
-           (candidate-str (s-trim (shell-command-to-string (format "git ls-files %s | grep -E '(^|/)%s'" relative (file-name-sans-extension target)))))
+           (candidate-str (s-trim (shell-command-to-string (format "git ls-files %s | grep -E '(^|/)%s\\.'" relative (file-name-sans-extension target)))))
            (candidate (--filter (not (string-equal target (f-filename it))) (--map (e:expand it path) (s-split "\n" candidate-str)))))
       (if (zerop (length candidate))
           (print "not found.")
