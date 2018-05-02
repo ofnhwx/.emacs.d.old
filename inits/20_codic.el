8d1021968e1881ba40bdad9da9f8728b4a28e8f8
Yuta Fujita <ofnhwx@komunan.net>
2018-05-02 15:34:21 +0900

    add: 'codic'を導入

---
+++ inits/20_codic.el
@@ -0,0 +1,11 @@
+;;; 20_codic.el --- configurations.
+;;; Commentary:
+;;; Code:
+
+(use-package codic
+  :ensure t
+  :config
+  (e:load-local-config "codic"))
+
+(provide '20_codic)
+;;; 20_codic.el ends here
