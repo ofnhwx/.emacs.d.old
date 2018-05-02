16d46e9234948e9621b7f7a113147c6d20febee1
Yuta Fujita <ofnhwx@komunan.net>
2018-05-02 18:58:37 +0900

    update: 設定を整理

--- init-config.el
+++ init-config.el
@@ -48,10 +48,6 @@
 ;;; 環境設定(共通)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; カスタムファイル
-(set-variable 'custom-file (e:expand "custom.el" :cache))
-;(load custom-file t)
-
 ;; 終了コマンドに別名
 (defalias 'exit 'save-buffers-kill-terminal)
 
@@ -60,71 +56,77 @@
           'executable-make-buffer-file-executable-if-script-p)
 
 ;; タイトルバーの書式
-(set-variable
- 'frame-title-format
- `("" invocation-name " " emacs-version "@" system-name " - "
-   (multiple-frames "[%F]: ")
-   (byte-compiled "(byte compiled) ")
-   (buffer-file-name "%f" "%b")))
-
-;; 日付の書式
-(set-variable 'display-time-format " %Y/%m/%d(%a) %H:%M")
-
-;; 認証ファイル
-(set-variable 'auth-sources `(,(e:expand "authinfo.plist" :config)))
-(set-variable 'plstore-encoded t)
+(set-variable 'frame-title-format
+              '((multiple-frames "[%F]:")
+                (buffer-file-name "%f" (dired-directory dired-directory "%b"))))
 
 ;; 行番号の表示
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 (setq-default display-line-numbers-width 4)
 
-;; GPG関連
-(set-variable 'epa-pinentry-mode 'loopback)
-
-;; カーソル位置を保存
+;; ファイルを移動
+;; [~/.emacs.d/cache]
+(set-variable 'auto-insert-directory (e:expand "insert" :cache))
+(set-variable 'bookmark-default-file (e:expand "bookmark" :cache))
+(set-variable 'custom-file (e:expand "custom.el" :cache))
 (set-variable 'save-place-file (e:expand "places" :cache))
-(save-place-mode 1)
-
-;; URLキャッシュ
+(set-variable 'server-auth-dir (e:expand "server" :cache))
+(set-variable 'tramp-persistency-file-name (e:expand "tramp" :cache))
 (set-variable 'url-cache-directory (e:expand "url" :cache))
+(set-variable 'url-cookie-file (e:expand "cookies" :cache))
+;; [~/.emacs.d/config]
+(set-variable 'auth-sources `(,(e:expand "authinfo.plist" :config)))
+(set-variable 'org-agenda-files (cl-remove-if 'file-directory-p (directory-files (e:expand "org/agenda" :config) t)))
+(set-variable 'org-directory (e:expand "org" :config))
 
 ;; さらに細かなもの
 (menu-bar-mode window-system) ;; コンソールではメニューバーを表示しない
-(tool-bar-mode        0) ;; ツールバーを非表示
-(tooltip-mode         0) ;; ツールチップを非表示
-(scroll-bar-mode      0) ;; スクロールバーを表示しない
-(show-paren-mode      1) ;; paren(対応する括弧のハイライト)を有効
-(blink-cursor-mode    0) ;; カーソルを点滅しない
-(save-place-mode      1) ;; カーソル位置を保存
-(global-hl-line-mode  0) ;; 下線を非表示(この辺りは'powerline'で対応)
-(size-indication-mode 0) ;; サイズを非表示
-(display-time-mode    0) ;; 時間を非表示
-(line-number-mode     0) ;; 行番号を非表示
-(column-number-mode   0) ;; 列番号を非表示
-(display-battery-mode 0) ;; バッテリー状態を非表示
-(set-variable 'gc-cons-threshold (* 128 1024 1024)) ;;
-(set-variable 'gc-cons-percentage 0.5)
-(set-variable 'max-specpdl-size     10000)  ;; 安全策
-(set-variable 'max-lisp-eval-depth  10000)  ;;
-(set-variable 'ring-bell-function 'ignore)  ;; ビープ音はなし
-(set-variable 'message-log-max     100000)  ;;
-(set-variable 'kill-ring-max          200)  ;;
-(setq-default  indent-tabs-mode       nil)  ;; インデントは空白
-(setq-default  tab-width              4)    ;; タブ幅は 4
-(setq-default  truncate-lines         t)    ;; 折り返さない
-(setq-default  bidi-display-reordering nil) ;; 右から左に読む言語に対応しない
-(set-variable 'truncate-partial-width-windows nil)
-(set-variable 'completion-ignore-case t)    ;; 補完で大文字小文字の区別をしない
-(set-variable 'inhibit-startup-screen t)    ;; スタートアップの画面を表示しない
-(set-variable 'resize-mini-windows    t)    ;; ミニバッファ拡大防止
-(set-variable 'mouse-drag-copy-region nil)  ;; 選択範囲を勝手にコピーしない
-(set-variable 'create-lockfiles       nil)  ;; lockfileを作成しない
-(set-variable 'require-final-newline  nil)  ;; 勝手に改行を追加しない
-(set-variable 'mode-require-final-newline nil)
+(auto-insert-mode 1)          ;; 新規ファイルでテンプレートを挿入
+(blink-cursor-mode 0)         ;; カーソルを点滅しない
+(column-number-mode 0)        ;; 列番号を非表示
+(display-battery-mode 0)      ;; バッテリー状態を非表示
+(display-time-mode 0)         ;; 時間を非表示
+(global-hl-line-mode 0)       ;; 下線を非表示
+(line-number-mode 0)          ;; 行番号を非表示
+(save-place-mode 1)           ;; カーソル位置を保存
+(scroll-bar-mode 0)           ;; スクロールバーを表示しない
+(show-paren-mode 1)           ;; paren(対応する括弧のハイライト)を有効
+(size-indication-mode 0)      ;; サイズを非表示
+(tool-bar-mode 0)             ;; ツールバーを非表示
+(tooltip-mode 0)              ;; ツールチップを非表示
+(set-variable 'completion-ignore-case t)                                ;; 補完で大文字小文字の区別をしない
+(set-variable 'create-lockfiles nil)                                    ;; lockfileを作成しない
+(set-variable 'ediff-split-window-function 'split-window-horizontally)  ;; `ediff'を横に並べて表示
+(set-variable 'ediff-window-setup-function 'ediff-setup-windows-plain)  ;; `ediff'でフレームを使用しない
+(set-variable 'epa-pinentry-mode 'loopback)                             ;; GnuPGでloopbackを指定
+(set-variable 'gc-cons-percentage 0.5)                                  ;; GC関連の調整
+(set-variable 'gc-cons-threshold (* 128 1024 1024))                     ;; 〃
+(set-variable 'inhibit-startup-screen t)                                ;; スタートアップの画面を表示しない
+(set-variable 'kill-ring-max 1000)                                      ;; `kill-ring'の最大数
+(set-variable 'max-lisp-eval-depth 10000)                               ;; 安全策
+(set-variable 'max-specpdl-size 10000)                                  ;; 〃
+(set-variable 'message-log-max 100000)                                  ;; `*Messages*'バッファの最大行数
+(set-variable 'mode-require-final-newline nil)                          ;; 勝手に改行を追加しない(`EditorConfig'で管理)
+(set-variable 'mouse-drag-copy-region nil)                              ;; 選択範囲を勝手にコピーしない
+(set-variable 'password-cache-expiry 3600)                              ;; パスワードをキャッシュしておく
+(set-variable 'plstore-encoded t)                                       ;; `plstore-mode'で暗号化する
+(set-variable 'require-final-newline nil)                               ;; `mode-require-final-newline'
+(set-variable 'resize-mini-windows t)                                   ;; ミニバッファ拡大防止
+(set-variable 'ring-bell-function 'ignore)                              ;; ビープ音はなし
+(set-variable 'truncate-partial-width-windows nil)                      ;; 折り返しは`truncate-lines'で制御
+(set-variable 'uniquify-buffer-name-style 'post-forward-angle-brackets) ;; バッファ名をユニークにする
+(setq-default bidi-display-reordering nil)                              ;; 右から左に読む言語に対応しない
+(setq-default indent-tabs-mode nil)                                     ;; インデントは空白
+(setq-default tab-width 4)                                              ;; タブ幅
+(setq-default truncate-lines t)                                         ;; 折り返さない
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 環境設定(個別)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;
+(when window-system
+  (add-hook 'after-init-hook 'server-start))
 
 ;; Linux/Mac用の設定
 (when (or (os-type-linux-p)
--- inits/60_hideshow.el
+++ inits/60_hideshow.el
@@ -5,12 +5,7 @@
 (use-package hideshow
   :diminish hs-minor-mode
   :init
-  (add-hook 'lisp-interaction-mode-hook 'hs-minor-mode)
-  (add-hook 'emacs-lisp-mode-hook 'hs-minor-mode)
-  (add-hook 'lisp-mode-hook 'hs-minor-mode)
-  (add-hook 'css-mode-hook 'hs-minor-mode)
-  (add-hook 'php-mode-hook 'hs-minor-mode)
-  (add-hook 'web-mode-hook 'hs-minor-mode))
+  (add-hook 'prog-mode-hook 'hs-minor-mode))
 
 (provide '60_hideshow)
 ;;; 60_hideshow.el ends here
