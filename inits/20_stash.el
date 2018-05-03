598d893baaf16a3b9862084012f481e59488b3f5
Yuta Fujita <ofnhwx@komunan.net>
2018-05-04 06:35:03 +0900

    add: 'stash'を導入

---
+++ inits/20_stash.el
@@ -0,0 +1,14 @@
+;;; 20_stash.el --- configurations.
+;;; Commentary:
+;;; Code:
+
+(use-package stash
+  :ensure t
+  :init
+  (set-variable 'stash-directory (e:expand "stashes" :cache))
+  :config
+  (defstashapp app 60)
+  (defstash kill-ring "kill-ring.el" app (or stashed 'nil)))
+
+(provide '20_stash)
+;;; 20_stash.el ends here
