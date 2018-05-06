952b844abb9a4b8fd94fc3d47a8b25cac2749a56
Yuta Fujita <ofnhwx@komunan.net>
2018-05-06 13:32:27 +0900

    add: 'maxframe'を導入

---
+++ inits/20_maxframe.el
@@ -0,0 +1,11 @@
+;;; 20_maxframe.el --- configurations.
+;;; Commentary:
+;;; Code:
+
+(use-package maxframe
+  :ensure t
+  :config
+  (maximize-frame))
+
+(provide '20_maxframe)
+;;; 20_maxframe.el ends here
