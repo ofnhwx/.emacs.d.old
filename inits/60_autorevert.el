bc02a76b985b56a63a0d9decd61d8301648bb1e3
Yuta Fujita <ofnhwx@komunan.net>
2018-05-21 15:04:52 +0900

    update: マイナーモードの表示を調整

---
+++ inits/60_autorevert.el
@@ -0,0 +1,9 @@
+;;; 60_autorevert.el --- configurations.
+;;; Commentary:
+;;; Code:
+
+(use-package autorevert
+  :diminish auto-revert-mode)
+
+(provide '60_autorevert)
+;;; 60_autorevert.el ends here
--- inits/60_hideshow.el
+++ inits/60_hideshow.el
@@ -3,8 +3,7 @@
 ;;; Code:
 
 (use-package hideshow
-  :diminish hs-minor-mode
-  :hook (prog-mode . hs-minor-mode))
+  :diminish hs-minor-mode)
 
 (provide '60_hideshow)
 ;;; 60_hideshow.el ends here
