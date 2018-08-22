16eb88852737abec94e5e41e7f1c22d261bd4006
Yuta Fujita <ofnhwx@komunan.net>
2018-08-22 15:50:43 +0900

    add: 'unicode-escape'を導入

---
+++ inits/20_unicode-escape.el
@@ -0,0 +1,10 @@
+;;; 20_unicode-escape.el --- configurations.
+;;; Commentary:
+;;; Code:
+
+(use-package unicode-escape
+  :ensure t
+  :demand t)
+
+(provide '20_unicode-escape)
+;;; 20_unicode-escape.el ends here
