851db8a88803885a769d1b215f5d3e4ae934109b
Yuta Fujita <ofnhwx@komunan.net>
2018-11-29 18:57:33 +0900

    update: ctagsの自動更新を開始する条件を調整

--- inits/30_ctags-update.el
+++ inits/30_ctags-update.el
@@ -6,7 +6,16 @@
   (executable-find "ctags")
   :ensure t
   :defer t
-  :hook (php-mode . turn-on-ctags-auto-update-mode))
+  :hook (php-mode . ctags-check-and-on)
+  :config
+  (defun ctags-check-and-on ()
+    (interactive)
+    (let ((path (file-name-directory buffer-file-name)))
+      (while (> (length path) 1)
+        (if (file-exists-p (e:expand "TAGS" path))
+            (progn (turn-on-ctags-auto-update-mode)
+                   (setq path nil))
+          (setq path (file-name-directory (directory-file-name path))))))))
 
 (provide '30_ctags-update)
 ;;; 30_ctags-update.el ends here
