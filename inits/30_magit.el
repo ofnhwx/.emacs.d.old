315812915c03eb84e3fde1496dc4e4dd86eeeec2
Yuta Fujita <ofnhwx@komunan.net>
2018-11-29 18:57:13 +0900

    update: magitでログを時系列に表示するオプションを追加

--- inits/30_magit.el
+++ inits/30_magit.el
@@ -8,7 +8,9 @@
   :custom
   (magit-log-margin '(t "%Y-%m-%d %H:%M" magit-log-margin-width t 15))
   (magit-diff-refine-hunk 'all)
-  (smerge-refine-ignore-whitespace nil))
+  (smerge-refine-ignore-whitespace nil)
+  :config
+  (magit-define-popup-switch 'magit-log-popup ?l "Always sort by date" "--date-order"))
 
 (use-package magit-gitflow
   :after (magit)
